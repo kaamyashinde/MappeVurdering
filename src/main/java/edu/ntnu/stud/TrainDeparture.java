62733f7efa185ce5b222d07ebdeace1ffc928d22
Kamya Shinde <kamyashinde@Kamyas-MacBook-Air.local>
2023-11-05 00:15:16 +0100

    Implement the Local Time class in the entity class and update the tests accordingly

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -1,50 +1,63 @@
 package edu.ntnu.stud;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
- * This file contains the class for the train departure.
- * The information that is to be displayed for
- * each train departure is the following:
- * - Departure Time - Train Line - Train ID -
- * Destination - Delayed Time - Track Number.
- * The class contains get-methods for all attributes, set-methods for the delay information and track attribute,
- * a to-string method and a static method to print out the header for the main table that will display the train departures.
+ * This file contains the class for the train departure. The information that is to be displayed for
+ * each train departure is the following: - Departure Time - Train Line - Train ID - Destination -
+ * Delayed Time - Track Number. The class contains get-methods for all attributes, set-methods for
+ * the delay information and track attribute, a to-string method and a static method to print out
+ * the header for the main table that will display the train departures.
  */
 public class TrainDeparture {
-  private final int DEPARTURE_TIME;
+  private final LocalTime DEPARTURE_TIME;
+  private final String departureTimeFormatted;
   private final String TRAIN_LINE;
   private final int TRAIN_ID;
   private final String DESTINATION;
   private int delay;
-  private int delayedTime;
+  private LocalTime delayedTime;
+  private String delayedTimeFormatted;
   private int track;
 
-  //Defining static String constants that will be used in the toString method to avoid repetition
-  static final String SEPARATOR = "------------------------------------------------------------------------------------";
+  // Defining static String constants that will be used in the toString method to avoid repetition
+  static final String SEPARATOR =
+      "------------------------------------------------------------------------------------";
   static final String HEADER =
-                    SEPARATOR + "\n"
-                  + "| Departure  | Train Line | Train ID   | Destination          | Delay Time | Track |" + "\n"
-                  + SEPARATOR;
+      SEPARATOR
+          + "\n"
+          + "| Departure  | Train Line | Train ID   | Destination          | Delay Time | Track |"
+          + "\n"
+          + SEPARATOR;
 
   // Constructor to initialize the train departure object
   public TrainDeparture(
-      int departureTime,
+      LocalTime departureTime,
       String trainLine,
       int trainId,
       String destination,
       int delay,
       int track) {
     this.DEPARTURE_TIME = departureTime;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    this.departureTimeFormatted = DEPARTURE_TIME.format(formatter);
     this.TRAIN_LINE = trainLine;
     this.TRAIN_ID = trainId;
     this.DESTINATION = destination;
     this.delay = delay;
-    this.delayedTime = DEPARTURE_TIME + delay;
+    this.delayedTime = DEPARTURE_TIME.plusMinutes(delay);
+    this.delayedTimeFormatted = delayedTime.format(formatter);
     this.track = track;
   }
 
   // get methods:
-  public int getDepartureTime() {
+  public LocalTime getDepartureTime() {
     return DEPARTURE_TIME;
+  }
+
+  public String getDepartureTimeFormatted() {
+    return departureTimeFormatted;
   }
 
   public String getTrainLine() {
@@ -63,8 +76,12 @@ public class TrainDeparture {
     return delay;
   }
 
-  public int getDelayedTime() {
+  public LocalTime getDelayedTime() {
     return delayedTime;
+  }
+
+  public String getDelayedTimeFormatted() {
+    return delayedTimeFormatted;
   }
 
   public int getTrack() {
@@ -74,15 +91,14 @@ public class TrainDeparture {
   // set methods:
   public void setDelayAndDelayTime(int delay) {
     this.delay = delay;
-    this.delayedTime = DEPARTURE_TIME + this.delay;
-
+    this.delayedTime = DEPARTURE_TIME.plusMinutes(delay);
   }
 
   public void setTrack(int track) {
     this.track = track;
   }
 
-  //Additional method that prints out the table header - will be used in the register methods
+  // Additional method that prints out the table header - will be used in the register methods
   public static String printTableHeader() {
     // Print the table header
     return HEADER;
@@ -92,32 +108,27 @@ public class TrainDeparture {
   @Override
   public String toString() {
 
-    if (track == -1){ //The scenario where there is no track number assigned to the train departure
-        return String.format(
-                // Define the format for each row of the table
-                "| %-10s | %-10s | %-10s | %-20s | %-10d | %-5s |",
-                DEPARTURE_TIME, TRAIN_LINE, TRAIN_ID, DESTINATION, delayedTime, " ")
-                + "\n"
-                + SEPARATOR;
-    } else if (delay == 0){ //The scenario where the train departure is on time
+    if (track
+        == -1) { // The scenario where there is no track number assigned to the train departure
       return String.format(
-              "| %-10s | %-10s | %-10s | %-20s | %-10s | %-5d |",
-              DEPARTURE_TIME, TRAIN_LINE, TRAIN_ID, DESTINATION, " ", track)
-              + "\n"
-              + SEPARATOR;
+              // Define the format for each row of the table
+              "| %-10s | %-10s | %-10d | %-20s | %-10s | %-5s |",
+              departureTimeFormatted, TRAIN_LINE, TRAIN_ID, DESTINATION, delayedTime, " ")
+          + "\n"
+          + SEPARATOR;
+    } else if (delay == 0) { // The scenario where the train departure is on time
+      return String.format(
+              "| %-10s | %-10s | %-10d | %-20s | %-10s | %-5d |",
+              departureTimeFormatted, TRAIN_LINE, TRAIN_ID, DESTINATION, " ", track)
+          + "\n"
+          + SEPARATOR;
     } else
-      return String.format( //The scenario where the train departure is delayed and has a track number assigned
-              "| %-10s | %-10s | %-10s | %-20s | %-10d | %-5d |",
-              DEPARTURE_TIME, TRAIN_LINE, TRAIN_ID, DESTINATION, delayedTime, track)
-              + "\n"
-              + SEPARATOR;
+      return String
+              .format( // The scenario where the train departure is delayed and has a track number
+                       // assigned
+                  "| %-10s | %-10s | %-10d | %-20s | %-10s | %-5d |",
+                  departureTimeFormatted, TRAIN_LINE, TRAIN_ID, DESTINATION, delayedTime, track)
+          + "\n"
+          + SEPARATOR;
   }
-
-
-
-
-
-
-
-
 }
--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -1,85 +1,121 @@
 package edu.ntnu.stud;
+
+import java.time.LocalTime;
 /**
- * This file runs JUnit tests on the TrainDeparture entity class to test the toString and printTableHeader methods.
- * The other methods aren't tested because they are getters and setters, and they are tested in the TrainDepartureRegisterTest class.
+ * This file runs JUnit tests on the TrainDeparture entity class to test the toString and
+ * printTableHeader methods. The other methods aren't tested because they are getters and setters,
+ * and they are tested in the TrainDepartureRegisterTest class.
  */
-
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class TrainDepartureTest {
-    TrainDeparture trainDeparture = new TrainDeparture(1, "test", 1, "test", 2,  1); //Delayed train departure that has a track
-    TrainDeparture trainDeparture1 = new TrainDeparture(1, "test", 1, "test", 0,  5); //Train Departure that is on time and has a track
-    TrainDeparture trainDeparture2 = new TrainDeparture(1, "test", 1, "test", 2,  -1); //Delayed train departure that does not have a track
-    @Test
-    void testToString() {
+  TrainDeparture trainDeparture =
+      new TrainDeparture(
+          LocalTime.of(15, 30),
+          "test",
+          1,
+          "test",
+          2,
+          1); // Delayed train departure that has a track
+  TrainDeparture trainDeparture1 =
+      new TrainDeparture(
+          LocalTime.of(15, 30),
+          "test",
+          1,
+          "test",
+          0,
+          5); // Train Departure that is on time and has a track
+  TrainDeparture trainDeparture2 =
+      new TrainDeparture(
+          LocalTime.of(15, 30),
+          "test",
+          1,
+          "test",
+          2,
+          -1); // Delayed train departure that does not have a track
 
+  @Test
+  void testToString() {
 
-        assertEquals("| 1          | test       | 1          | test                 | 3          | 1     |\n" +
-                "------------------------------------------------------------------------------------", trainDeparture.toString()); //Testing the printing of all attributes
-        assertEquals("| 1          | test       | 1          | test                 |            | 5     |\n" +
-                "------------------------------------------------------------------------------------", trainDeparture1.toString()); //Since it is not delayed, a blank space should be printed for the delayed time
-        assertEquals("| 1          | test       | 1          | test                 | 3          |       |\n" +
-                "------------------------------------------------------------------------------------", trainDeparture2.toString()); //Since it does not have a track, a blank space should be printed for the track
-
-      }
+    assertEquals(
+        "| 15:30      | test       | 1          | test                 | 15:32      | 1     |\n"
+            + "------------------------------------------------------------------------------------",
+        trainDeparture.toString()); // Testing the printing of all attributes
+    assertEquals(
+        "| 15:30      | test       | 1          | test                 |            | 5     |\n"
+            + "------------------------------------------------------------------------------------",
+        trainDeparture1
+            .toString()); // Since it is not delayed, a blank space should be printed for the
+                          // delayed time
+    assertEquals(
+        "| 15:30      | test       | 1          | test                 | 15:32      |       |\n"
+            + "------------------------------------------------------------------------------------",
+        trainDeparture2
+            .toString()); // Since it does not have a track, a blank space should be printed for the
+                          // track
+  }
 
   @Test
   void printTableHeader() {
     assertEquals(TrainDeparture.HEADER, TrainDeparture.printTableHeader());
   }
 
+  // testing the get-methods:
+  @Test
+  void getDepartureTimeFormatted() {
+    // if this test works then the method for getDepartureTime() works as well
+    // because if it hadn't then it wouldn't have had converted the right value to String
+    assertEquals("15:30", trainDeparture.getDepartureTimeFormatted());
+  }
 
-    //testing the get-methods:
-    @Test
-    void getDepartureTime() {
-        assertEquals(1, trainDeparture.getDepartureTime());
-      }
+  @Test
+  void getTrainLine() {
+    assertEquals("test", trainDeparture.getTrainLine());
+  }
 
-    @Test
-    void getTrainLine() {
-        assertEquals("test", trainDeparture.getTrainLine());
-      }
+  @Test
+  void getTrainId() {
+    assertEquals(1, trainDeparture.getTrainId());
+  }
 
-    @Test
-    void getTrainId() {
-        assertEquals(1, trainDeparture.getTrainId());
-      }
+  @Test
+  void getDestination() {
+    assertEquals("test", trainDeparture.getDestination());
+  }
 
-    @Test
-    void getDestination() {
-        assertEquals("test", trainDeparture.getDestination());
-      }
+  @Test
+  void getDelay() {
+    assertEquals(2, trainDeparture.getDelay());
+    assertEquals(
+        0, trainDeparture1.getDelay()); // Testing the case where the train departure is on time
+  }
 
-    @Test
-    void getDelay() {
-        assertEquals(2, trainDeparture.getDelay());
-        assertEquals(0, trainDeparture1.getDelay()); //Testing the case where the train departure is on time
-      }
+  @Test
+  void getDelayedTimeFormatted() {
+      // if this test works then the method for getDelayedTime() works as well
+      // because if it hadn't then it wouldn't have had converted the right value to String
+    assertEquals("15:32", trainDeparture.getDelayedTimeFormatted());
+  }
 
-    @Test
-    void getDelayedTime() {
-        assertEquals(3, trainDeparture.getDelayedTime());
-      }
+  @Test
+  void getTrack() {
+    assertEquals(1, trainDeparture.getTrack());
+    assertEquals(
+            -1, trainDeparture2.getTrack()); // Testing the case where the train departure does not have a track
+  }
 
-    @Test
-    void getTrack() {
-        assertEquals(1, trainDeparture.getTrack());
-        assertEquals(-1, trainDeparture2.getTrack()); //Testing the case where the train departure does not have a track
-      }
+  // testing the set-methods:
+  @Test
+  void setDelay() {
+    trainDeparture.setDelayAndDelayTime(2);
+    assertEquals(2, trainDeparture.getDelay());
+  }
 
-
-    //testing the set-methods:
-    @Test
-    void setDelay() {
-        trainDeparture.setDelayAndDelayTime(2);
-        assertEquals(2, trainDeparture.getDelay());
-      }
-
-    @Test
-    void setTrack() {
-        trainDeparture.setTrack(2);
-        assertEquals(2, trainDeparture.getTrack());
-      }
+  @Test
+  void setTrack() {
+    trainDeparture.setTrack(2);
+    assertEquals(2, trainDeparture.getTrack());
+  }
 }
