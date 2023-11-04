480f2a11a607e4b52e537e4aeac04c4ac8b00320
Kamya Shinde <kamyashinde@Kamyas-MacBook-Air.local>
2023-11-05 00:16:02 +0100

    Update the TrainDepartureRegister Test based on the implementation of local Time class in the entity class

--- src/test/java/edu/ntnu/stud/TrainDepartureRegisterTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureRegisterTest.java
@@ -5,15 +5,17 @@ package edu.ntnu.stud;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 class TrainDepartureRegisterTest {
 
     @Test
     void addTrainDeparture() {
         TrainDepartureRegister tdr = new TrainDepartureRegister();
-        TrainDeparture trainDepartureNumber1 = new TrainDeparture(1, "test", 1, "test", 1, 1, 1);
+        TrainDeparture trainDepartureNumber1 = new TrainDeparture(LocalTime.of(15,30), "test", 1, "test", 1, 1);
         tdr.addTrainDeparture(trainDepartureNumber1);
-        TrainDeparture trainDepartureNumber2 = new TrainDeparture(2, "test", 2, "test", 2, 2, 2);
+        TrainDeparture trainDepartureNumber2 = new TrainDeparture(LocalTime.of(15,30), "test", 2, "test", 2, 2);
         tdr.addTrainDeparture(trainDepartureNumber2);
 
         assertEquals(2, tdr.allTrainDepartures.size());
@@ -23,7 +25,7 @@ class TrainDepartureRegisterTest {
     @Test
     void setDelayThroughRegister() {
         TrainDepartureRegister tdr = new TrainDepartureRegister();
-        TrainDeparture trainDepartureNumber1 = new TrainDeparture(1, "test", 1, "test", 1, 1, 1);
+        TrainDeparture trainDepartureNumber1 = new TrainDeparture(LocalTime.of(15,30), "test", 1, "test", 1, 1);
         tdr.addTrainDeparture(trainDepartureNumber1);
         tdr.setDelayThroughRegister(1, 2);
 
@@ -33,7 +35,7 @@ class TrainDepartureRegisterTest {
     @Test
     void setTrackNumberThroughRegister() {
         TrainDepartureRegister tdr = new TrainDepartureRegister();
-        TrainDeparture trainDepartureNumber1 = new TrainDeparture(1, "test", 1, "test", 1, 1, 1);
+        TrainDeparture trainDepartureNumber1 = new TrainDeparture(LocalTime.of(15,30), "test", 1, "test", 1, 1);
         tdr.addTrainDeparture(trainDepartureNumber1);
         tdr.setTrackNumberThroughRegister(1, 2);
 
@@ -43,8 +45,8 @@ class TrainDepartureRegisterTest {
     @Test
     void testToString() {
         TrainDepartureRegister tdr = new TrainDepartureRegister();
-        TrainDeparture trainDepartureNumber1 = new TrainDeparture(1, "test", 1, "test", 1, 1, 1);
-        TrainDeparture trainDepartureNumber2 = new TrainDeparture(2, "test", 2, "test", 2, 2, 2);
+        TrainDeparture trainDepartureNumber1 = new TrainDeparture(LocalTime.of(15,30), "test", 1, "test", 1, 1);
+        TrainDeparture trainDepartureNumber2 = new TrainDeparture(LocalTime.of(15,30), "test", 2, "test", 2, 2);
         tdr.addTrainDeparture(trainDepartureNumber1);
         tdr.addTrainDeparture(trainDepartureNumber2);
 
